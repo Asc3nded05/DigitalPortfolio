e1de085cd628c73280599f8eebeb0de761abcad9
Riley Van Heukelum <rileyvanheukelum@gmail.com>
2026-09-07 22:31:00 -0400

    React Router error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
@@ -11,23 +11,21 @@ import Contact from './pages/Contact'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex min-h-screen flex-col">
-        <Header />
+    <div className="flex min-h-screen flex-col">
+      <Header />
 
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/process" element={<Process />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/process" element={<Process />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
 
-        <Footer />
-      </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   )
 }
 
